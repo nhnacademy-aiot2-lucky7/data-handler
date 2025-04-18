bf3fa534a7aaf32b699017d025716c581d7a86a6
nink2458 <nink2458@gmail.com>
2025-04-18 11:26:15 +0900

    init

---
+++ src/main/java/com/nhnacademy/parser/SpringIotDataParserApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.parser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIotDataParserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringIotDataParserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nhnacademy/parser/SpringIotDataParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.parser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIotDataParserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
