bc68398b9a0504415b1c6c1b72f595a12f93c44f
nink2458 <nink2458@gmail.com>
2025-04-22 13:55:07 +0900

    수정 사항: `DataParser`의 전체적인 로직이 수정 되었습니다. 이제 File 기반 뿐만 아니라, String 타입의 문자열 데이터도 Parsing 할 수 있습니다.

---
+++ src/main/java/com/nhnacademy/data/parser/impl/BinaryDataParser.java
@@ -0,0 +1,29 @@
+package com.nhnacademy.data.parser.impl;
+
+import com.nhnacademy.data.parser.DataParser;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 추후 구현 예정...
+ */
+public class BinaryDataParser implements DataParser {
+
+    @Override
+    public String getFileType() {
+        return "";
+    }
+
+    @Override
+    public List<Map<String, Object>> parsing(String payload) throws IOException {
+        return List.of();
+    }
+
+    @Override
+    public List<Map<String, Object>> parsing(File file) throws IOException {
+        return List.of();
+    }
+}
