8d348027c75b97cb599f1c44c109c58dd5f5de3f
nink2458 <nink2458@gmail.com>
2025-05-02 17:30:54 +0900

    변경 사항: `RuleData` DTO 구조를 개선

--- src/main/java/com/nhnacademy/rule/dto/RuleData.java
+++ src/main/java/com/nhnacademy/rule/dto/RuleData.java
@@ -1,49 +1,40 @@
 package com.nhnacademy.rule.dto;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public final class RuleData {
 
     /**
      * 게이트웨이 ID
      */
+    @JsonProperty("gateway_id")
     private final String gatewayId;
 
     /**
      * 센서 ID
      */
+    @JsonProperty("sensor_id")
     private final String sensorId;
 
     /**
      * 센서가 측정하는 데이터 타입
      */
+    @JsonProperty("type")
     private final String type;
 
     /**
      * 센서가 데이터를 측정한 값
      */
+    @JsonProperty("value")
     private final Double value;
 
     /**
      * 센서가 데이터를 측정한 시간대
      */
+    @JsonProperty("timestamp")
     private final Long timestamp;
-
-    @JsonCreator
-    public RuleData(
-            @JsonProperty("gateway-id") String gatewayId,
-            @JsonProperty("sensor-id") String sensorId,
-            @JsonProperty("type") String type,
-            @JsonProperty("value") Double value,
-            @JsonProperty("timestamp") Long timestamp
-    ) {
-        this.gatewayId = gatewayId;
-        this.sensorId = sensorId;
-        this.type = type;
-        this.value = value;
-        this.timestamp = timestamp;
-    }
 }
