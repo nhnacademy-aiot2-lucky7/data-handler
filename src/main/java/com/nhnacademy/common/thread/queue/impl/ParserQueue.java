93e9b5682085110c8dbfd79d7f46a05252f179e6
nink2458 <nink2458@naver.com>
2025-05-05 03:35:54 +0900

    변경 사항: 각 `Queue`에서 수행하던 공통된 기능들을 `TaskQueue`에게 구현의 책임을 넘기도록 구조 변경

---
+++ src/main/java/com/nhnacademy/common/thread/queue/impl/ParserQueue.java
@@ -0,0 +1,27 @@
+package com.nhnacademy.common.thread.queue.impl;
+
+import com.nhnacademy.common.thread.properties.ParserThreadPoolProperties;
+import com.nhnacademy.common.thread.queue.TaskQueue;
+import com.nhnacademy.common.thread.runnable.ParserTask;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * Parser가 처리할 작업들을 저장하고, 순차적으로 꺼내어 <br>
+ * 실행할 수 있도록 관리하는 대기열 클래스입니다.
+ * <hr>
+ *
+ * @see ParserTask
+ */
+@Component
+public final class ParserQueue extends TaskQueue {
+
+    public ParserQueue(
+            ParserThreadPoolProperties properties,
+            @Qualifier("parserTaskRunning") AtomicBoolean running
+    ) {
+        super(properties.getCapacity(), running);
+    }
+}
